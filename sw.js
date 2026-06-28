c8a668adac9881b21ebc8d4caeef241788f395a8
Ellie Huang <280650564+elliehuang37-jpg@users.noreply.github.com>
2026-06-28 16:17:33 +0800

    Add root Pages entrypoint

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "nlp-meditation-trainer-21-v5";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/meditation-icon.png",
+  "./icons/apple-touch-icon.png",
+  "./assets/brand-mark.png",
+  "./assets/sun-cloud-header.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
